35dd730ce7c20ad271e8c41a30895665f08e2d4b
Jacques Thibodeau <thibo.jacques@gmail.com>
2022-07-06 22:31:07 +0000

    Created script for generating completions with cpu and gpu.

--- test_gpt.py
+++ test_gpt.py
@@ -1,20 +1,61 @@
 import torch
 from transformers import AutoModelForCausalLM
 from transformers import AutoTokenizer
+from time import time
+import argparse
+import numpy as np
+
+
+def generate(input_str, model, tokenizer, device, length=50, n=1):
+    cur_ids = torch.tensor(tokenizer.encode(input_str)).unsqueeze(0).long().to(device)
+    model.eval()
+    with torch.no_grad():
+        for i in range(length):
+            outputs = model(cur_ids[:, -1024:], labels=cur_ids[:, -1024:])
+            loss, logits = outputs[:2]
+            softmax_logits = torch.softmax(logits[0,-1], dim=0)
+            next_token_id = choose_from_top(softmax_logits.to('cpu').numpy(), n=n)
+            cur_ids = torch.cat([cur_ids, torch.ones((1,1)).long().to(device) * next_token_id], dim=1)
+        output_list = list(cur_ids.squeeze().to('cpu').numpy())
+        output_text = tokenizer.decode(output_list)
+        return output_text.replace("<|endoftext|>", "")
+
+
+def choose_from_top(probs, n=1):
+    ind = np.argpartition(probs, -n)[-n:]
+    top_prob = probs[ind]
+    top_prob = top_prob / np.sum(top_prob) # Normalize
+    choice = np.random.choice(n, 1, p = top_prob)
+    token_id = ind[choice][0]
+    return int(token_id)
 
 def test_gpt(text="Today is a nice day"):
+
+    argparser = argparse.ArgumentParser()
+    argparser.add_argument("--text", type=str, default=text)
+    argparser.add_argument("--num_return_sequences", type=int, default=1)
+    argparser.add_argument("--gpu", type=bool, default=False)
+
+    args = argparser.parse_args()
+    if args.gpu:
+        device = torch.device("cuda")
+    else:
+        device = torch.device("cpu")
+
+    print(f"Using device: {device}.")
+
     gpt2 = AutoModelForCausalLM.from_pretrained("gpt2", return_dict_in_generate=True)
+    gpt2 = gpt2.to(device)
     tokenizer = AutoTokenizer.from_pretrained("gpt2")
 
     input_ids = tokenizer(text, return_tensors="pt").input_ids
 
-    generated_outputs = gpt2.generate(input_ids, do_sample=True, num_return_sequences=1, output_scores=True)
-
-    # only use id's that were generated
-    # gen_sequences has shape [3, 15]
-    gen_sequences = generated_outputs.sequences[:, input_ids.shape[-1]:]
+    start = time()
+    generated_text = generate(text, gpt2, tokenizer, device, n=args.num_return_sequences)
+    end = time()
+    print("Time it took to generate tokens:", end - start)
 
     print("Generated completion: \n")
-    print(text + str(tokenizer.decode(gen_sequences[0])))
+    print(generated_text)
 
 test_gpt()
