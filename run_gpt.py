caa4bebab9fa24d8f95f63926ca2749048362ecb
Jacques Thibodeau <thibo.jacques@gmail.com>
2022-07-08 20:15:59 +0000

    Removed unused functions.

--- run_gpt.py
+++ run_gpt.py
@@ -7,30 +7,6 @@ import numpy as np
 import pandas as pd
 
 
-def generate(input_str, model, tokenizer, device, length=50, n=1):
-    encoding = tokenizer("Today is a nice day", return_tensors="pt")
-    cur_ids = torch.tensor(encoding).unsqueeze(0).long().to(device)
-    model.eval()
-    with torch.no_grad():
-        for i in range(length):
-            outputs = model(cur_ids[:, -1024:], labels=cur_ids[:, -1024:])
-            loss, logits = outputs[:2]
-            softmax_logits = torch.softmax(logits[0,-1], dim=0)
-            next_token_id = choose_from_top(softmax_logits.to('cpu').numpy(), n=n)
-            cur_ids = torch.cat([cur_ids, torch.ones((1,1)).long().to(device) * next_token_id], dim=1)
-        output_ids = list(cur_ids.squeeze().to('cpu').numpy())
-        output_text = tokenizer.decode(output_ids)
-        return output_text.replace("<|endoftext|>", ""), output_ids
-
-
-def choose_from_top(probs, n=1):
-    ind = np.argpartition(probs, -n)[-n:]
-    top_prob = probs[ind]
-    top_prob = top_prob / np.sum(top_prob) # Normalize
-    choice = np.random.choice(n, 1, p = top_prob)
-    token_id = ind[choice][0]
-    return int(token_id)
-
 def test_gpt():
 
     argparser = argparse.ArgumentParser()
